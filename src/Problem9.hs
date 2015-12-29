3e5de88aa01f649ec69348fb0472fca9a19fc5d0
Striking <Strikingwolf2012@gmail.com>
2015-12-28 19:00:08 -0600

    Refactor Problem 9
    Use euler's method on pythagorean triples, much more efficient

--- src/Problem9.hs
+++ src/Problem9.hs
@@ -1,6 +1,10 @@
 module Problem9 where
 
-pythagTriplesOfSum n = filter (\(a, b, c) -> a^2 + b^2 == c^2) [(a, b, c) | a <- [1..n], b <- [1..n], c <- [1..n], a + b + c == n]
+primitives = [(m^2 - n^2, 2 * m * n, m^2 + n^2) | m <- [1..], n <- [1..(m - 1)], (m - n) `mod` 2 == 1, gcd m n == 1]
+
+pythagTriplesOfSum n = [let
+  x =  n `div` (a + b + c)
+  in (x * a, x * b, x * c) | (a, b, c) <- primitives, n `mod` (a + b + c) == 0]
 
 tripleProduct :: (Int, Int, Int) -> Int
 tripleProduct (a, b, c) = a * b * c
